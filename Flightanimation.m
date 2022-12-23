e4eb41fca228acc2a5d5a6e820fd1aca4d1305af
akshaydhame2001 <89789189+akshaydhame2001@users.noreply.github.com>
2022-12-23 12:19:20 +0530

    Add files via upload

---
+++ Flightanimation.m
@@ -0,0 +1,11 @@
+clc
+clear all
+close all
+load simdata
+Time = simdata(:,1)
+X = simdata(:,[1,2])
+Y = simdata(:,[1,3])
+Z = simdata(:,[1,4])
+Roll = simdata(:,[1,5])
+Pitch = simdata(:,[1,6])
+Yaw = simdata(:,[1,7])
